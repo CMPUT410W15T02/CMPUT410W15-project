e5d9162450da2c8c0878b313142967c5f661834c
David Chau <davidchau10@gmail.com>
2015-03-04 22:08:13 -0700

    Friend Requests

--- social_distribution/authors/urls.py
+++ social_distribution/authors/urls.py
@@ -8,5 +8,10 @@ urlpatterns = patterns('',
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^author/$', views.author, name='author'),
     url(r'^manage/$', views.author_manage, name='author_manage'),
+
+	url(r'^profile/(?P<username>\w+)/$',views.profile,name='profile'),
+    url(r'^friend_request/$', views.friend_request, name='friend_request'),
+    url(r'^add_friend/$', views.add_friend, name='add_friend'),
+    
     url(r'^$', views.index, name='index')
 )
