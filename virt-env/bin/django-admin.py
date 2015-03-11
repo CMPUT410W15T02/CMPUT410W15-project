42c875dac060eb5023a418806f12e262a6b16aac
rooshilp <rooshilp@gmail.com>
2015-03-11 16:42:35 -0600

    recreated virtual env as testenv

--- virt-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/frank/Desktop/socialdistribution/CMPUT410W15-project/virt-env/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
