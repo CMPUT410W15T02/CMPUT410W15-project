ef9a1784d55c0bf83eb712de8f203eb0503f783a
Rooshil Patel <rooshilp@gmail.com>
2015-03-24 20:20:50 -0600

    added more to HTTP auth for API

--- social_distribution/posts/api_auth.py
+++ social_distribution/posts/api_auth.py
@@ -10,16 +10,7 @@ import base64
 # By schinckel
 class APIAuthMiddleware(object):
 	def process_request(self, request):
-		if not request.path.startswith('/api/'):
-			return
-
-		if request.META.has.key('HTTP_AUTHORIZATION'):
-			auth = request.META['HTTP_AUTHORIZATION'].split()
-			if len(auth) == 2:
-				if auth[0].lower() == 'basic':
-					uname, host, passwd = base64.b64decode(auth[1]).split(':')
-
-		response = HttpResponse()
+				response = HttpResponse()
 		response.status_code = 401
 		try:
 			realm = settings.HTTP_AUTH_REALM
@@ -27,6 +18,26 @@ class APIAuthMiddleware(object):
 			realm = ""
 
 		response['WWW-Authenticate'] = 'Basic realm ="%s"' % realm
+
+		if not request.path.startswith('/api/'):
+			return
+
+		if request.META.has.key('HTTP_AUTHORIZATION'):
+			auth = request.META['HTTP_AUTHORIZATION'].split()
+		else:
+			return response
+			
+		try:
+			uname, host, passwd = base64.b64decode(auth[1]).split(':')
+		except:
+			return response
+
+		if not Host.objects.filter(url=host).exists():
+			return response
+
+		if password != 'testpass':
+			return response
+
 		return response
 		
 
